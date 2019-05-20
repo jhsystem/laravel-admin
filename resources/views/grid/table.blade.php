da71c08a8b731088b0635ac07e34feeeb2ee7261
juhun lee <leeyaho@gmail.com>
2019-05-20 10:29:38 +0900

    fix click detail view

--- resources/views/grid/table.blade.php
+++ resources/views/grid/table.blade.php
@@ -1,55 +1,54 @@
 <div class="box">
     @if(isset($title))
-    <div class="box-header with-border">
-        <h3 class="box-title"> {{ $title }}</h3>
-    </div>
+        <div class="box-header with-border">
+            <h3 class="box-title"> {{ $title }}</h3>
+        </div>
     @endif
 
     @if ( $grid->showTools() || $grid->showExportBtn() || $grid->showCreateBtn() )
-    <div class="box-header with-border">
-        <div class="pull-right">
-            {!! $grid->renderColumnSelector() !!}
-            {!! $grid->renderExportButton() !!}
-            {!! $grid->renderCreateButton() !!}
-        </div>
-        @if ( $grid->showTools() )
-        <span>
+        <div class="box-header with-border">
+            <div class="pull-right">
+                {!! $grid->renderColumnSelector() !!}
+                {!! $grid->renderExportButton() !!}
+                {!! $grid->renderCreateButton() !!}
+            </div>
+            @if ( $grid->showTools() )
+                <span>
             {!! $grid->renderHeaderTools() !!}
         </span>
-        @endif
-    </div>
+            @endif
+        </div>
     @endif
 
     {!! $grid->renderFilter() !!}
 
     {!! $grid->renderHeader() !!}
 
-    <!-- /.box-header -->
+<!-- /.box-header -->
     <div class="box-body table-responsive no-padding">
         <table id="main_data" class="table table-hover table-bordered" @if($grid->getWidth()) style="width: {{ $grid->getWidth() }}px;" @endif>
             <thead>
-                <tr>
-                    @foreach($grid->visibleColumns() as $column)
+            <tr>
+                @foreach($grid->visibleColumns() as $column)
                     <th>{{$column->getLabel()}}{!! $column->sorter() !!}</th>
-                    @endforeach
-                </tr>
+                @endforeach
+            </tr>
             </thead>
 
             <tbody>
-                @foreach($grid->rows() as $row)
+            @foreach($grid->rows() as $row)
                 <tr {!! $row->getRowAttributes() !!} >
-
                     @foreach($grid->visibleColumnNames() as $name)
-                    <td {!! $row->getColumnAttributes($name) !!} @if($name == '__actions__'){{ "class='act'" }}@endif>
-                        @if($name == '__actions__')
-                            <div class="act">{!! $row->column($name) !!}</div>
-                        @else
-                        {!! $row->column($name) !!}
-                        @endif
-                    </td>
+                        <td {!! $row->getColumnAttributes($name) !!} >
+                            @if($name == '__actions__')
+                                <div class="act">{!! $row->column($name) !!}</div>
+                            @else
+                                {!! $row->column($name) !!}
+                            @endif
+                        </td>
                     @endforeach
                 </tr>
-                @endforeach
+            @endforeach
             </tbody>
         </table>
 
@@ -60,16 +59,20 @@
     <div class="box-footer clearfix">
         {!! $grid->paginator() !!}
     </div>
-        <script>
-            $('div.act').click(function (e) {
-                e.stopPropagation();
-            });
-            $('tr').click(function () {
+    <script>
+        var stop = false;
+        $('div.act').click(function (e) {
+            stop = true;
+        });
+        $('tr').click(function () {
+            if(stop) {
+                stop = false;
+            } else {
                 var href = $(this).find('a:first-child');
-                if(href)
+                if (href)
                     href[0].click();
-            });
-
-        </script>
+            }
+        })
+    </script>
     <!-- /.box-body -->
 </div>
